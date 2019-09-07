966679ce946e228bff8dffce66c95849214f9f69
Tom <tsechrist@outlook.com>
2019-09-07 12:48:07 -0500

    App.js includes components

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Book from "./components/Book"
+import Header from "./components/Header"
+import Saved from "./components/Saved"
+import Search from "./components/Search"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+
+        <Header />
+        <Search />
+        <Saved />
+        <Book />
+
+        {/* <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }
